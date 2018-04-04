0083eeff0637ce62f52b95208c48882e1e62445a
Shehtab Zaman <shehtabzaman@gmail.com>
2018-04-04 13:11:00 -0400

    Updated to discount garabge values when no devices on board

--- Hello_World/device_check.cu
+++ Hello_World/device_check.cu
@@ -6,23 +6,26 @@ https://devblogs.nvidia.com/how-query-device-properties-and-handle-errors-cuda-c
 using namespace std;
 int main(int argc, char const *argv[]) {
   /* code */
-  int nDevices;
+  int nDevices = 0;
 
   cudaGetDeviceCount(&nDevices);
   //sets nDevices to the number of CUDA capable devices (GPUs)
 
-  cout << nDevices << '\n';
-  // for (int i = 0; i < nDevices; i++) {
-  //   cudaDeviceProp prop;
-  //   cudaGetDeviceProperties(&prop, i);
-  //
-  //   cout << "Device Numer: " << i <<"\n";
-  //   cout <<"\t"<< "Device Name:"<<prop.name<<"\n";
-  //   cout <<"\t"<< "Clock Rate(KHz):"<<prop.memoryClockRate<<"\n";
-  //   cout <<"\t"<< "But Width(bits):"<<prop.memoryBusWidth<<"\n";
-  //   cout << "\t" << "Memory Bandwidth(GB/s):" <<
-  //   2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << "\n";
-  // }
+  cout <<"Total Devices: "<< nDevices << '\n';
+
+    for (int i = 0; i < nDevices; i++) {
+      cudaDeviceProp prop;
+      cudaGetDeviceProperties(&prop, i);
+
+      cout << "Device Numer: " << i <<"\n";
+      cout <<"\t"<< "Device Name:"<<prop.name<<"\n";
+      cout <<"\t"<< "Clock Rate(KHz):"<<prop.memoryClockRate<<"\n";
+      cout <<"\t"<< "But Width(bits):"<<prop.memoryBusWidth<<"\n";
+      cout << "\t" << "Memory Bandwidth(GB/s):" <<
+      2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6 << "\n";
+    }
+
+
 
   return 0;
 }
